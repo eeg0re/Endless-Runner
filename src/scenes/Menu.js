d46bb4aa8ec305e13a88b4daf02085fa3d697850
Igor Bessa <ibessa@ucsc.edu>
2023-10-27 11:45:35 -0700

    saving progress on skeletons

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,11 +8,23 @@ class Menu extends Phaser.Scene{
     }
 
     create(){
+        let menuConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+        }
 
+        
     }
 
     update(){
-        
+
     }
 
 }
