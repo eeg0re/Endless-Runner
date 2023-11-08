5ebc4e5157a4de6a136aa315dd83ba821543fd23
Igor Bessa <ibessa@ucsc.edu>
2023-11-07 23:17:28 -0800

    finished credit scenes

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -12,16 +12,21 @@ class Credits extends Phaser.Scene{
 
         let creditConfig = {
             fontFamily: 'Courier',
-            fontSize: '18px',
+            fontSize: '12px',
             backgroundColor: '#424e7a',
             color: '#f0f1f5',
-            align: 'center',
+            align: 'left',
             padding: {
                 top: 5,
                 bottom: 5,
             },
         }
         this.mountain = this.add.tileSprite(0,0, game.config.width, game.config.height, 'mountains').setOrigin(0,0);
+        this.add.text(10, 20, 'Play scene music is "8 Bit Surf"\nby David Renda on Fesliyan Studios', creditConfig);
+        this.add.text(10, 60, 'UI sound effect is by Lesiakower on Pixabay,\nhttps://pixabay.com/sound-effects/search/8-bit/', creditConfig);
+        this.add.text(10, 100, 'Game Over sound effect is by Pixabay,\nhttps://pixabay.com/sound-effects/search/game-over/', creditConfig);
+        this.add.text(10, 140, 'Jump sound effect by Pixabay,\nhttps://pixabay.com/sound-effects/search/8-bit%20jump/', creditConfig);
+        this.add.text(10, 180, 'Menu scene music is "beam" by sinneschlösen on Pixabay,\nhttps://pixabay.com/music/search/8bit/', creditConfig);
 
     }
 
