e1923242441c4121e9743cd5a4afeb0566806b4d
Igor Bessa <ibessa@ucsc.edu>
2023-12-20 18:51:26 -0800

    Changed credit scene font and text

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -10,25 +10,11 @@ class Credits extends Phaser.Scene{
     create(){
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        let creditConfig = {
-            fontFamily: 'Courier',
-            fontSize: '12px',
-            backgroundColor: '#424e7a',
-            color: '#f0f1f5',
-            align: 'left',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-        }
         this.mountain = this.add.tileSprite(0,0, game.config.width, game.config.height, 'mountains').setOrigin(0,0);
-        this.add.text(10, 20, 'Play scene music is "8 Bit Surf"\nby David Renda on Fesliyan Studios', creditConfig);
-        this.add.text(10, 60, 'UI sound effect is by Lesiakower on Pixabay,\nhttps://pixabay.com/sound-effects/search/8-bit/', creditConfig);
-        this.add.text(10, 100, 'Game Over sound effect is by Pixabay,\nhttps://pixabay.com/sound-effects/search/game-over/', creditConfig);
-        this.add.text(10, 140, 'Jump sound effect by Pixabay,\nhttps://pixabay.com/sound-effects/search/8-bit%20jump/', creditConfig);
-        this.add.text(10, 180, 'Menu scene music is "beam" by sinneschlösen on Pixabay,\nhttps://pixabay.com/music/search/8bit/', creditConfig);
+        this.add.bitmapText(width/2, height-100, 'menu-font', 'Gameplay music by David Renda on Fesliyan Studios').setOrigin(0.5).setScale(0.2);
+        this.add.bitmapText(width/2, height-60, 'menu-font', 'Menu music and sound effects by Pixabay').setOrigin(0.5).setScale(0.2);
+        this.add.bitmapText(width/4, height-30, 'menu-font', 'Press Space to return').setOrigin(0.5).setScale(0.2);
 
-        this.add.text(10, 260, 'Press space to return', creditConfig);
 
     }
 
